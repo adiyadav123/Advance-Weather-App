d79497cda33216f9ce3b6ab1c8ba682af773ac82
Aditya Yadav <ay971876@gmail.com>
2023-10-09 07:52:38 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey = 'c1b724d4a8b26f8f2bf3bd5cbd950b10';
+// const geoApi = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}'
+// const weatherApi = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}';
