0ea0a6007d80ca2ad40162f4876f4508e5e7d4c4
Aditya Yadav <ay971876@gmail.com>
2023-10-09 10:26:58 -0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const apiKey = 'c1b724d4a8b26f8f2bf3bd5cbd950b10';
-// const geoApi = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}'
-// const weatherApi = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}';
+let search_button = document.querySelector(".search");
+let apiKey = 'c1b724d4a8b26f8f2bf3bd5cbd950b10';
+let searchInput = document.querySelector("#text");
+
+
+search_button.addEventListener("click", async() => {
+    let city = searchInput.value;
+    if(!city){
+        return Swal.fire(
+            'City is required'
+          )
+    } 
+    
+    let geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+    let geoRes = await fetch(geoApi);
+    let geoData = await geoRes.json();
+
+    if(geoData.length < 1){
+        return Swal.fire(
+            'City not found.'
+          )
+    }
+
+    let city_lon = geoData[0].lon;
+    let city_lat = geoData[0].lat;
+
+    let weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${city_lat}&lon=${city_lon}&appid=${apiKey}&units=metric`;
+    let weatherRes = await fetch(weatherApi);
+    let weatherData = await weatherRes.json();
+
+    console.log(weatherData);
+
+
+});
